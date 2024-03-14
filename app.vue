47a9fed15c29ccff895616c550ca2dd82af3c5c7
mirsella <mirsella@protonmail.com>
2024-03-14 19:53:55 +0100

    add nuxtjs/seo and test appwriter

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
+<script setup lang="ts">
+const appwrite = useAppwrite();
+// let a = await account.create(
+//   ID.unique(),
+//   "test@gmail.com",
+//   "65ef4ce532070e537d98",
+//   "test user",
+// );
+// await account.createEmailSession("test@gmail.com", "65ef4ce532070e537d98");
+// let a = await account.get();
+// console.log(a);
+const account = useState("account", () => {});
+try {
+  console.log("account.get");
+  account.value = await appwrite.account.get();
+} catch (e) {
+  console.error("account.get", e);
+  await navigateTo("/login");
+}
+</script>
+
 <template>
   <div>
-    <h1 class="size-40 i-ms-manage-accounts">test</h1>
+    <NuxtPage />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-appwrite", "@nuxtjs/seo"],
   devtools: { enabled: true },
-  // for mobile capacitor
   ssr: false,
+  appwrite: { project: process.env.APPWRITE_PROJECT },
+  site: {
+    url: process.env.SITE_URL || "http://localhost:3000",
+    name: "Inspire",
+    description: "quality classes to improve your life",
+    defaultLocale: "fr",
+  },
 });
