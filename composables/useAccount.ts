17e8bf0a8d5d8dc0c079bcc4f9b4f01fe99d9a5c
mirsella <mirsella@protonmail.com>
2024-03-25 11:49:23 +0100

    fix useaccount

--- composables/useAccount.ts
+++ composables/useAccount.ts
@@ -3,17 +3,18 @@ import type { Models } from "nuxt-appwrite";
 let call: any = null;
 export const useAccount = async () => {
   const appwrite = useAppwrite();
-  if (!call) {
-    call = appwrite.account.get();
-  }
   const account = useState(
     "account",
     () => null as Models.User<Models.Preferences> | null,
   );
-  try {
-    account.value = await call;
-  } catch {
-    account.value = null;
+  if (!call) {
+    try {
+      call = appwrite.account.get();
+      account.value = await call;
+    } catch {
+      account.value = null;
+    }
   }
+  await call;
   return account;
 };
