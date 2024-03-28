9891d9d42d9fa7bb080b81a153974dc0ed801229
mirsella <mirsella@protonmail.com>
2024-03-28 20:23:00 +0100

    subscription_updated now update the users document with the lm user id

--- functions/subscription_updated/src/index.ts
+++ functions/subscription_updated/src/index.ts
@@ -16,12 +16,6 @@ export default async ({ req, res, log, error }: Context) => {
     throw new Error("missings env variables");
   }
 
-  const client = new Client()
-    .setEndpoint("https://cloud.appwrite.io/v1")
-    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(process.env.APPWRITE_API_KEY);
-  const users = new Users(client);
-
   if (req.method !== "POST") {
     return res.send("only supporting POST", 405);
   }
@@ -32,26 +26,47 @@ export default async ({ req, res, log, error }: Context) => {
     return res.send("only supporting subscription_updated event", 405);
   }
 
-  log(JSON.stringify(req.body));
+  const client = new Client()
+    .setEndpoint("https://cloud.appwrite.io/v1")
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(process.env.APPWRITE_API_KEY);
+  const users = new Users(client);
+  const databases = new Databases(client);
+
   const email = req.body.data.attributes.user_email;
   let userslist = await users.list([Query.equal("email", email)]);
   let user: Models.User<Models.Preferences>;
   if (userslist.total === 1) {
-    let user = userslist.users[0];
+    user = userslist.users[0];
   } else if (userslist.total === 0) {
-    let user = await users.create(ID.unique(), email);
+    user = await users.create(ID.unique(), email);
+    // wait for the user to be created in the `users` collection by the users functions
+    log("waiting for the users document to be created");
+    while (true) {
+      try {
+        await databases.getDocument("classes", "users", user.$id);
+        break;
+      } catch {
+        await new Promise((resolve) => setTimeout(resolve, 100));
+      }
+    }
   } else {
     throw new Error("multiple users with same email");
   }
+  log("finished retrieving/creating the user");
 
-  // @ts-ignore the value is initialized in the if/else block
   let labels = user.labels;
   if (req.body.data.attributes.status === "active") {
     labels.push("premium");
-    await users.updateLabels(req.headers["x-appwrite-user-id"], labels);
+    await databases.updateDocument("classes", "users", user.$id, {
+      lemonsqueezy_id: req.body.data.attributes.customer_id,
+    });
+    await users.updateLabels(user.$id, labels);
   } else if (req.body.data.attributes.status === "expired") {
     labels.splice(labels.indexOf("premium"), 1);
-    await users.updateLabels(req.headers["x-appwrite-user-id"], labels);
+    await users.updateLabels(user.$id, labels);
   }
-  log(`User ${req.headers["x-appwrite-user-id"]} updated labels: ${labels}`);
+  log(
+    `User ${req.headers["x-appwrite-user-id"]} updated labels: ${labels} becauses of subscription status: ${req.body.data.attributes.status}`,
+  );
 };
