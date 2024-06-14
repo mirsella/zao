d4507f695af7e5e598e696fbb7370df221fe59ae
mirsella <mirsella@protonmail.com>
2024-06-15 00:58:10 +0200

    add text to home page

--- pages/index.vue
+++ pages/index.vue
@@ -10,8 +10,8 @@ definePageMeta({
 
 <template>
   <div class="mx-auto max-w-[80%]">
-    <div class="flex flex-wrap md:flex-nowrap gap-6 mx-auto p-2 items-center">
-      <div class="md:w-1/3">
+    <div class="flex flex-wrap lg:flex-nowrap gap-6 mx-auto p-2 items-center">
+      <div class="lg:w-1/3">
         <p class="text-5xl py-6 font-bold">
           Les podcasts qui vont transformer votre vie.
         </p>
@@ -24,30 +24,84 @@ definePageMeta({
           <span class="i-carbon-arrow-right size-6" />
         </NuxtLink>
       </div>
-      <Carousel class="md:w-2/3" />
+      <Carousel class="w-full lg:w-2/3" />
     </div>
-    <div class="grid grid-cols-1 m-6 gap-10">
-      <div class="card bg-base-200 shadow-xl">
+
+    <div class="grid grid-cols-1 my-10 gap-10">
+      <div class="card lg:card-side bg-base-200 shadow-md">
+        <figure>
+          <img src="/assets/owl.svg" class="aspect-square size-36" />
+        </figure>
         <div class="card-body">
-          <p class="card-title">Title 1</p>
-          <p class="card-body">bla bla</p>
-        </div>
-      </div>
-      <div class="card bg-base-200 shadow-xl">
-        <div class="card-body">
-          <p class="card-title">Applications IOS & Android</p>
+          <p class="card-title">histoires</p>
           <p class="card-body">
-            retrouvez toutes vos vidéos n'importe ou avec la lecture en arrière
-            plan et téléchargement hors ligne.
+            retrouver tous les jours des nouvelles histoires d'environ 20minutes
+            etc...
           </p>
         </div>
       </div>
-      <div class="card bg-base-200 shadow-xl">
+      <div class="card lg:card-side bg-base-200 shadow-md">
         <div class="card-body">
-          <p class="card-title">7€ par mois.</p>
-          <div class="card-body">
-            <p>pour un engagement de 12 mois.</p>
-            <p>9€/mois sans engagement.</p>
+          <p class="card-title">sante mental</p>
+          <p class="card-body">
+            de petits audios de quelques minutes qui vous aident à vous etc...
+          </p>
+        </div>
+        <figure>
+          <img src="/assets/owl.svg" class="aspect-square size-36" />
+        </figure>
+      </div>
+      <div class="card bg-base-200 shadow-md">
+        <div class="card-body">
+          <div class="card-title text-2xl justify-center">
+            ou et quand vous voulez.
+          </div>
+          <div class="card-body !gap-8 flex-nowrap">
+            <div class="w-min">
+              <div class="mockup-phone">
+                <div class="camera"></div>
+                <div class="display">
+                  <div
+                    class="artboard artboard-demo phone-1 bg-base-200 flex justify-center px-4 py-16"
+                  >
+                    <div class="card card-compact text-center">
+                      <p class="card-title justify-center">
+                        ✔️ hors connection
+                      </p>
+                      <p class="card-body">
+                        telecharger vos capsules sur votre smartphone et ecouter
+                        les a tout moment
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="card card-compact text-center">
+              <p class="card-title justify-center">✔️ hors connection</p>
+              <p class="card-body">
+                telecharger vos capsules sur votre smartphone et ecouter les a
+                tout moment
+              </p>
+            </div>
+
+            <div class="mockup-browser border border-base-300">
+              <div class="mockup-browser-toolbar">
+                <div class="input border border-base-300">https://zao.com</div>
+              </div>
+              <div
+                class="flex justify-center px-4 py-16 border-t border-base-300"
+              >
+                <div class="card card-compact">
+                  <p class="card-title">✔️ sur tous les supports</p>
+                  <p class="card-body">
+                    Retrouvez le meilleur de l'audio en accès illimité sur votre
+                    smartphone, tablette et ordinateur.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
