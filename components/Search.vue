3240b46db194834d29bc1d4fcbb623e868301f50
mirsella <mirsella@protonmail.com>
2024-06-20 22:33:30 +0200

    create a separate search component, improve search

---
+++ components/Search.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import type { Pod } from "~";
+
+const props = defineProps<{ pods: Pod[] }>();
+const emit = defineEmits(["filtered"]);
+const tags = computed(() =>
+  Array.from(
+    new Set(props.pods.flatMap((pod) => pod.tags).filter((tag) => tag.length)),
+  ),
+);
+const selectedtags = ref<string[]>([]);
+const search = ref("");
+
+watchEffect(() => {
+  const filtered = props.pods.filter((pod) => {
+    if (
+      selectedtags.value.length &&
+      !selectedtags.value.some((tag) => pod.tags.includes(tag))
+    ) {
+      return false;
+    }
+    if (search.value.length) {
+      const searchLower = search.value.toLowerCase();
+      if (
+        !pod.title.toLowerCase().includes(searchLower) &&
+        !pod.description.toLowerCase().includes(searchLower) &&
+        !pod.key_points.some((point) =>
+          point.toLowerCase().includes(searchLower),
+        ) &&
+        !pod.tags.some((tag) => tag.toLowerCase().includes(searchLower))
+      ) {
+        return false;
+      }
+    }
+    return true;
+  });
+  emit("filtered", filtered);
+});
+
+function toggleTag(tag: string) {
+  const index = selectedtags.value.indexOf(tag);
+  if (index !== -1) {
+    selectedtags.value.splice(index, 1);
+  } else {
+    selectedtags.value.push(tag);
+  }
+}
+</script>
+<template>
+  <div>
+    <label
+      class="input input-secondary flex items-center w-full max-w-lg mx-auto"
+    >
+      <input
+        v-model.trim="search"
+        class="grow text-lg"
+        placeholder="Recherche..."
+      />
+      <button
+        class="i-carbon-close size-6"
+        v-show="search.length !== 0"
+        @click="search = ''"
+      ></button>
+    </label>
+    <div class="w-full flex items-center justify-center flex-wrap gap-2 mt-6">
+      <h1 class="text-xl inline-block md:m-4">Catégories:</h1>
+      <button
+        v-for="tag in tags"
+        :class="{
+          'bg-secondary text-secondary-content hover:bg-secondary':
+            selectedtags.includes(tag),
+        }"
+        class="btn btn-sm mx-px hover:scale-105 transition text-lg"
+        @click="toggleTag(tag)"
+      >
+        {{ tag.toLowerCase() }}
+      </button>
+    </div>
+  </div>
+</template>
--- pages/capsules/index.vue
+++ pages/capsules/index.vue
@@ -2,93 +2,31 @@
 import type { Pod } from "~";
 
 useHeadSafe({ title: "Bibliothèque" });
-const search = ref("");
-const pods = ref<Pod[]>([]);
-const tags = ref<string[]>([]);
-const selectedtags = ref<string[]>([]);
 
-usePodcasts().then((data) => {
-  pods.value = data.value.filter((pod) => pod.type === "capsule");
-  tags.value = Array.from(
-    new Set(pods.value.flatMap((pod) => pod.tags).filter((tag) => tag.length)),
-  );
-});
-
-// TODO: better search with more
-const podsfiltered = computed(() =>
-  pods.value.filter((pod) => {
-    if (
-      selectedtags.value.length &&
-      !selectedtags.value.some((tag) => pod.tags.includes(tag))
-    ) {
-      return false;
-    }
-    if (search.value.length) {
-      const searchLower = search.value.toLowerCase();
-      if (
-        !pod.title.toLowerCase().includes(searchLower) &&
-        !pod.description.toLowerCase().includes(searchLower) &&
-        !pod.key_points.some((point) =>
-          point.toLowerCase().includes(searchLower),
-        )
-      ) {
-        return false;
-      }
-    }
-    return true;
-  }),
+const pods = ref<Pod[]>(
+  (await usePodcasts()).value.filter((pod) => pod.type === "capsule"),
 );
-
-function toggleTag(tag: string) {
-  const index = selectedtags.value.indexOf(tag);
-  if (index !== -1) {
-    selectedtags.value.splice(index, 1);
-  } else {
-    selectedtags.value.push(tag);
-  }
-}
+const podsfiltered = ref<Pod[]>([...pods.value]);
 </script>
 
 <template>
-  <div class="flex flex-col gap-4 items-center max-w-7xl mx-auto">
-    <label class="input input-secondary flex items-center w-full max-w-lg">
-      <input
-        v-model.trim="search"
-        class="grow text-lg"
-        placeholder="Recherche..."
+  <div>
+    <Search
+      class="my-6"
+      :pods="pods"
+      @filtered="(pods) => (podsfiltered = pods)"
+    />
+    <div
+      v-if="pods.length"
+      class="mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-14 md:max-w-6xl justify-items-center mx-auto"
+    >
+      <Poster
+        :title="true"
+        v-for="pod in podsfiltered"
+        :pod="pod"
+        class="text-xl w-96 md:w-80 lg:w-72 xl:w-80 px-8"
       />
-      <button
-        class="i-carbon-close size-6"
-        v-show="search.length !== 0"
-        @click="search = ''"
-      ></button>
-    </label>
-    <template v-if="pods.length">
-      <div class="w-full flex items-center justify-center flex-wrap gap-2">
-        <h1 class="text-xl inline-block md:m-4">Catégories:</h1>
-        <button
-          v-for="tag in tags"
-          :class="{
-            'bg-secondary text-secondary-content hover:bg-secondary':
-              selectedtags.includes(tag),
-          }"
-          class="btn btn-sm mx-px hover:scale-105 transition text-lg"
-          @click="toggleTag(tag)"
-        >
-          {{ tag.toLowerCase() }}
-        </button>
-      </div>
-      <div
-        class="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-14 md:max-w-6xl justify-items-center"
-      >
-        <Poster
-          :title="true"
-          v-for="pod in podsfiltered"
-          :pod="pod"
-          class="text-xl w-96 md:w-80 lg:w-72 xl:w-80 px-8"
-        />
-      </div>
-    </template>
+    </div>
     <span
       v-else
       class="loading loading-infinity text-secondary loading-lg mt-10"
