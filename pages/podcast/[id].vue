c06c0e29b7210723df1fc1cb525a3a5e895ef1ce
mirsella <mirsella@protonmail.com>
2024-07-14 15:03:52 +0200

    improve style of podcast page

--- pages/podcast/[id].vue
+++ pages/podcast/[id].vue
@@ -7,9 +7,9 @@ const pod = ref<Pod>();
 usePodcasts().then((pods) => {
   pod.value = pods.value.find((pod) => pod.$id === route.params.id);
   if (!pod.value) {
-    showError("ce podcast n'existe pas");
+    return showError("ce podcast n'existe pas");
   }
-  useHeadSafe({ title: pod.value?.title });
+  useHeadSafe({ title: pod.value.title });
 });
 
 function play() {
@@ -19,37 +19,35 @@ function play() {
 </script>
 
 <template>
-  <div class="max-w-5xl mx-auto p-6">
-    <div class="space-y-10" v-if="pod">
-      <div class="md:flex md:space-x-20">
-        <div class="w-60 md:w-1/3 mx-auto pb-5 md:pb-0">
-          <Poster :pod="pod" :hidetitle="true" class="max-h-[70%]" />
-        </div>
-        <div class="flex-1 text-center md:text-left">
-          <hr class="hidden md:block border-primary border-t-2 w-9 pb-1" />
-          <p class="text-md font-normal pb-2 capitalize">
-            {{ pod.type === "daily" ? "histoire" : "story" }}
-          </p>
-          <p class="text-2xl font-bold">{{ pod.title }}</p>
-          <div class="bg-base-200 p-6 mt-6 rounded-xl space-y-5">
-            <p class="text-xl font-bold">Vous allez découvrir :</p>
-            <div
-              v-for="point in pod.key_points"
-              class="flex items-center text-cloud text-lg"
-            >
-              <span class="i-carbon-checkmark size-6 px-6"></span>
-              {{ point }}
-            </div>
+  <div v-if="pod" class="max-w-5xl mx-auto p-6">
+    <div class="md:flex md:space-x-20">
+      <div class="w-60 md:w-1/3 mx-auto">
+        <Poster :pod="pod" :hidetitle="true" class="max-h-[90%]" />
+      </div>
+      <div class="flex-1 text-center md:text-left">
+        <hr class="hidden md:block border-primary border-t-2 w-9 pb-1" />
+        <p class="text-md font-normal pb-2 capitalize">
+          {{ pod.type === "daily" ? "histoire" : "story" }}
+        </p>
+        <p class="text-2xl mb-4 font-bold">{{ pod.title }}</p>
+        <div class="bg-base-200 my-4 p-6 rounded-xl space-y-5">
+          <p class="text-xl font-bold">Vous allez découvrir :</p>
+          <div
+            v-for="point in pod.key_points"
+            class="flex items-center text-cloud text-lg"
+          >
+            <span class="i-carbon-checkmark size-6 px-6"></span>
+            {{ point }}
           </div>
         </div>
+        <div class="btn my-4 w-full md:btn-wide text-lg" @click="play()">
+          écouter
+          <span class="i-carbon-play size-6"></span>
+        </div>
       </div>
-      <div class="text-lg font-normal text-center md:text-left">
-        {{ pod.description }}
-      </div>
-      <div class="btn w-full md:btn-wide text-lg" @click="play()">
-        écouter
-        <span class="i-carbon-play size-6"></span>
-      </div>
+    </div>
+    <div class="text-lg font-normal text-center md:text-left">
+      {{ pod.description }}
     </div>
   </div>
 </template>
